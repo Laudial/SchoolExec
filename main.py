0c824cf6fd955a0dff8872518b19b0a0ef75f3be
Laudial <laudial.dev@gmail.com>
2023-11-13 10:40:54 +0400

    Add croissant() & decroissant()

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
 import os
 import random
 
+
 def clear():
     os.system("cls" if os.name == "nt" else "clear")
 
+
 listA = [random.randint(-10, 10) for i in range(10)]
 
-def noSort(listB):
+
+def croissant(listB):
     for i in range(len(listB)):
         listMin = i
 
         for j in range(i + 1, len(listB)):
             if listB[j] < listB[listMin]:
-               listMin = j
+                listMin = j
 
         listB[listMin], listB[i] = listB[i], listB[listMin]
 
     return listB
 
+
+def decroissant(listB):
+    for i in range(len(listB)):
+        listMin = i
+
+        for j in range(i + 1, len(listB)):
+            if listB[j] > listB[listMin]:
+                listMin = j
+
+        listB[listMin], listB[i] = listB[i], listB[listMin]
+
+    return listB
+
+
 clear()
 
-print(noSort(listA))
+print("trie par odre croissant", croissant(listA))
+print("trie par odre décroissant", decroissant(listA))
